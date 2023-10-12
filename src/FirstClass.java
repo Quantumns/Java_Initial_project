816e5ab825efcbeb00a1274ece307c9b9e80accf
Enes <enes.aydin.bsns@gmail.com>
2023-10-12 20:11:48 +0200

    third update

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,6 +1,6 @@
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.println("Hello World this is test 3");
+        System.out.println("Hello World this is test");
 
     }
 }
