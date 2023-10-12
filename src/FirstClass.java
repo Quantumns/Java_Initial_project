ddee248baa82bf30d765c10b61ab66147bcbab99
Enes <enes.aydin.bsns@gmail.com>
2023-10-12 20:12:11 +0200

    4th update

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,6 +1,6 @@
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.println("Hello World this is test");
+        System.out.println("Hello World this is 4th");
 
     }
 }
